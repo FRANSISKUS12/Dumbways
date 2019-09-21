424fff32a109b954895d2273b27579f2a38b0dcb
FRANSISKUS12 <55622728+FRANSISKUS12@users.noreply.github.com>
2019-09-21 18:24:06 +0700

    Add files via upload

---
+++ nomor_4.js
@@ -0,0 +1,12 @@
+function dw(input){
+  console.log('D                      ') 
+  console.log('  U                    ')
+  console.log('    M                  ')
+  console.log('      B                ')
+  console.log('        W              ')
+  console.log('          A            ')
+  console.log('            Y          ') 
+  console.log('              S        ')
+
+}
+dw();
